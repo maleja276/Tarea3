fdf791782bbd85c144ddf83a3e4bd58827dce21b
Alejandra <ubuntu@alejandra>
2017-07-17 14:41:53 -0500

    Propagacion de Onda con la barrera

--- Onda.py
+++ Onda.py
@@ -1,5 +1,8 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+#import sys
+#sys.path.append('../')
+#from mascarasXY import mascaras
 
 #Solucionar la euacion de Onda en dos dimensiones 
 
@@ -31,6 +34,11 @@ u_pasado=np.zeros([n,n])
 m_tiempo=[]
 
 def evolucion(t_max):
+
+	barrera = np.ones((302,302))
+	barrera[200][0:140]=0
+	barrera[200][160:302]=0
+
 	for i in range (1,300):
 
 		for j in range (1,300):
@@ -39,7 +47,7 @@ def evolucion(t_max):
 
 	m_tiempo.append(u_inicial)
 
-	u_presente=u_futuro.copy()
+	u_presente=u_futuro.copy()*barrera
 
 	u_pasado=u_inicial.copy()
 
@@ -51,26 +59,26 @@ def evolucion(t_max):
 			
 				u_futuro[i][j]= ((u_presente[i+1][j]-2*u_presente[i][j]+u_presente[i-1][j])*(r**2))+((r**2)*(u_presente[i][j+1]-2*u_presente[i][j]+u_presente[i][j-1]))+2*u_presente[i][j]-u_pasado[i][j]
 
+
 		u_pasado=u_presente.copy()
-		u_presente=u_futuro.copy()
+		u_presente=u_futuro.copy()*barrera
 		m_tiempo.append(u_presente)
 
 
 	return u_presente, m_tiempo
 
-plt.imshow(evolucion(30)[0], cmap="binary")
+it_30=30/dt
+it_60=60/dt
+
+plt.imshow(evolucion(int(it_30))[0], cmap="binary")
 plt.title("Evolucion de la onda t=30s")
 plt.savefig("Onda_30.pdf")
 plt.close()
 
-plt.imshow(evolucion(60)[0], cmap="binary")
+plt.imshow(evolucion(int(it_60))[0], cmap="binary")
 plt.title("Evolucion de la onda t=60s")
 plt.savefig("Onda_60.pdf")
 plt.close()
 
-
-
-
-
-
+fig=plt.figure(figsize=(15,15))
 

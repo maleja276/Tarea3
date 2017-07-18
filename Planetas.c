e2e70d1e3b790f23f89e51afa7a7704d89311b64
Alejandra <ubuntu@alejandra>
2017-07-18 11:56:35 -0500

    Lectura de datos, masa en unidades solares

--- Planetas.c
+++ Planetas.c
@@ -4,24 +4,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#define PI 3.141519
+#define G 39.476568
+#define n 1000
 
-int n = 1000;
 
-int posicion(int i,int j){
+
+//Posicion en terminos lineales dado un i,j
+
+int indices(int i,int j){
 	return n*j+1;
 }
 
+
+
 int main(void){
+	
+	//Definicion de variables
 
 	FILE *datos;
 
 	int i=0, j=0; 
-	int fil=10, col=6;	
-	int matriz_datos[fil][col];
+	int fil=10, col=7;	
+	float matriz_datos[fil][col];
 	int let=250;
 	char lineas[let];
 	char *palabras;
 	const char *delimiter;
+	
+	float masa_sol;
 
 	delimiter=","; 
 
@@ -29,23 +40,39 @@ int main(void){
 
 	
 	while(fgets(lineas, let, datos)){
-
+		j=0;
 		palabras=strtok(lineas,delimiter);
 
 		while (palabras!=NULL){
-
-			matriz_datos[i][j]=atof(palabras);
-			//printf("%s\n", palabras);
+			if(j!=0){
+				matriz_datos[i][j-1]=atof(palabras);
+			}
 			palabras=strtok(NULL,delimiter);
-//			i+=1;
+			j+=1;
+			
 		}
-//		j+=1;
-//	printf("%d",matriz_datos[0][0]);
+		i+=1;		
+	}
+	
+	float m_sol = matriz_datos[0][0];
 
+	for (i=0; i<10; i++){
+
+		matriz_datos[i][0]= matriz_datos[i][0]/ m_sol;
 		
-	}	
+	}
 
-	float *p_x= malloc(n*fil*sizeof(float));
+/*	for (i=0;i<10;i++){
+
+		for(j=0;j<col;j++)
+			{
+			printf("%e\n", matriz_datos[i][j]);
+			}
+	}
+
+/*	//Deficion Matrices
+
+	/*float *p_x= malloc(n*fil*sizeof(float));
 	float *p_y= malloc(n*fil*sizeof(float));
 	float *p_z= malloc(n*fil*sizeof(float));
 
@@ -56,6 +83,10 @@ int main(void){
 	float *vm_x= malloc(fil*sizeof(float));
 	float *vm_y= malloc(fil*sizeof(float));
 	float *vm_z= malloc(fil*sizeof(float));
+
+
+	
+	}*/
 
 }
 

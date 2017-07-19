4cf91225046196285f0caaecf43b01dc8cb03863
Alejandra <ubuntu@alejandra>
2017-07-18 20:59:12 -0500

    primer planteamiento funcion aceleracion

--- Planetas.c
+++ Planetas.c
@@ -4,18 +4,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #define PI 3.141519
 #define G 39.476568
 #define n 1000
 
+//Definir las funciones 
 
+void aceleracion(int i, int t, float *ax, float *ay, float *az, float matriz_datos, float *p_x, float *p_y, float *p_z);
+int cambio_indices(int i,int j);
 
 //Posicion en terminos lineales dado un i,j
-
-int indices(int i,int j){
-	return n*j+1;
-}
-
 
 
 int main(void){
@@ -31,6 +30,23 @@ int main(void){
 	char lineas[let];
 	char *palabras;
 	const char *delimiter;
+
+	//Deficion Matrices
+
+	float *ax;
+	float *ay;
+	float *az;
+	float *p_x= malloc(n*fil*sizeof(float));
+	float *p_y= malloc(n*fil*sizeof(float));
+	float *p_z= malloc(n*fil*sizeof(float));
+
+	float *v_x= malloc(n*fil*sizeof(float));
+	float *v_y= malloc(n*fil*sizeof(float));
+	float *v_z= malloc(n*fil*sizeof(float));
+
+	float *vm_x= malloc(fil*sizeof(float));
+	float *vm_y= malloc(fil*sizeof(float));
+	float *vm_z= malloc(fil*sizeof(float));
 	
 	float masa_sol;
 
@@ -70,19 +86,7 @@ int main(void){
 			}
 	}
 
-/*	//Deficion Matrices
-
-	/*float *p_x= malloc(n*fil*sizeof(float));
-	float *p_y= malloc(n*fil*sizeof(float));
-	float *p_z= malloc(n*fil*sizeof(float));
-
-	float *v_x= malloc(n*fil*sizeof(float));
-	float *v_y= malloc(n*fil*sizeof(float));
-	float *v_z= malloc(n*fil*sizeof(float));
-
-	float *vm_x= malloc(fil*sizeof(float));
-	float *vm_y= malloc(fil*sizeof(float));
-	float *vm_z= malloc(fil*sizeof(float));
+/*
 
 
 	
@@ -90,5 +94,33 @@ int main(void){
 
 }
 
+int cambio_indices(int i,int j){
+	
+	return n*j+1;
+}
+
+
+void aceleracion(int i, int t, float *ax, float *ay, float *az, float matriz_datos, float *p_x, float *p_y, float *p_z)
+
+{
+	float constante;
+	float masa;
+	int ii=0;
+
+	for(ii=0;ii<10;ii++){
+
+		if(ii!=i){
+
+			masa=matriz_datos[ii][0];
+			constante= (G * masa)/pow((pow((p_x[ii,t]-p_x[i,t]),2.0)-pow((p_y[ii,t]-p_y[i,t]),2.0)-pow((p_z[ii,t]-p_z[i,t]),2.0)),(3.0/2.0));
+
+
+			}
+	}
+	
+	ax=constante * (p_x[ii,t]-p_x[i,t]);
+	ay=constante * (p_y[ii,t]-p_y[i,t]);
+	az=constante * (p_z[ii,t]-p_z[i,t]);
+}
 
 
